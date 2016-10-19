187db7fcc0741b323be31cdb1739b44722be073c
Costas100 <constantineathanitis@yahoo.com>
2016-10-19 16:11:59 -0400

    finished getAverages

--- get_averages.py
+++ get_averages.py
@@ -3,9 +3,32 @@ import sqlite3
 db = sqlite3.connect("newDB.db")
 c = db.cursor()
 
-s = "SELECT name, students.id, mark FROM students, courses WHERE students.id = courses.id"
+cmd = "SELECT name, courses.id, mark FROM students, courses WHERE students.id = courses.id"
 
-t = c.execute(s)
+sel = c.execute(cmd)
 
-for record in t:
-    print record
+name = "kruder"
+iD = 1
+num=0.0
+sUM=0.0
+
+
+for record in sel:
+    if (record[0] == name and iD == record[1]):
+        
+        num = num + 1
+        sUM = sUM + record[2]
+ 
+    else:
+        print "Name: " + name
+        print "ID: " + str(iD)
+        print "Average: " + str(sUM/num)
+        print "======================"
+        name = record[0]
+        iD = record[1]
+        num=1.0
+        sUM=record[2]
+
+print "Name: " + name
+print "ID: " + str(iD)
+print "Average: " + str(sUM/num)
